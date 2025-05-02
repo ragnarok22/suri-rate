c0d04c606b010f5befc708116f39943b442e4daa
Reinier Hernández <sasuke.reinier@gmail.com>
2025-05-02 11:15:23 -0300

    perf(cron): migrate rates endpoint to Next.js App Router and update authentication

---
+++ app/api/cron/rates/route.ts
@@ -0,0 +1,12 @@
+export async function GET(request: Request) {
+  const authHeader = request.headers.get("authentication");
+  console.log(authHeader);
+  console.log(process.env.CRON_SECRET);
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
+  return Response.json({ success: true });
+}
