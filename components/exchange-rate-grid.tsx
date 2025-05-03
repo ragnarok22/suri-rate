c1a2e495832a89d1cf9b94fca7bea7fdad966a86
Reinier Hernández <sasuke.reinier@gmail.com>
2025-05-03 17:25:51 -0300

    perf(cron-job): optimize exchange rate data fetching and rendering

--- components/exchange-rate-grid.tsx
+++ components/exchange-rate-grid.tsx
@@ -1,21 +1,23 @@
 import ExchangeRateCard from "./exchange-rate-card";
 import { getCurrentRates } from "@/utils/places";
 import { findBestRates } from "@/utils";
+import { getRates } from "@/utils/data";
 
 export default async function ExchangeRateGrid() {
-  const bankRates = await getCurrentRates();
+  const bankRates = await getRates();
   // Find best rates
-  const bestRates = findBestRates(bankRates);
+  const bestRates = findBestRates(bankRates || []);
 
   return (
     <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4">
-      {bankRates.map((bankRate) => (
-        <ExchangeRateCard
-          key={bankRate.name}
-          bankRates={bankRate}
-          bestRates={bestRates}
-        />
-      ))}
+      {bankRates &&
+        bankRates.map((bankRate) => (
+          <ExchangeRateCard
+            key={bankRate.name}
+            bankRates={bankRate}
+            bestRates={bestRates}
+          />
+        ))}
     </div>
   );
 }
